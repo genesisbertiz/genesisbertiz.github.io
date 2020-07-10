c06c6464f54b32f53d07a6ae9fc3c7382fd8e084
genesis <genesis@email.com>
2020-07-10 16:35:22 +0800

    Fix publishing url

---
+++ precache-manifest.a192aae0117e1b3b661b59563423b8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94d57f59893c5ad563d1c6bf19729ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcbe69f0a9de9779e1e0",
+    "url": "/static/css/main.4a93caba.chunk.css"
+  },
+  {
+    "revision": "fb1805f3e2e10eec8193",
+    "url": "/static/js/2.56f38295.chunk.js"
+  },
+  {
+    "revision": "41178ad4d23c29bfaea6636d448ad3aa",
+    "url": "/static/js/2.56f38295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbe69f0a9de9779e1e0",
+    "url": "/static/js/main.88167669.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "37602b50bb5a7d86820949bdd322ef5f",
+    "url": "/static/media/snowflake.37602b50.svg"
+  }
+]);
